b2c4c364ae028dee2611a65d1537e68e7987b030
Quang Lam <quang.lam2807@gmail.com>
2017-04-26 00:44:56 +0000

    610.2

---
+++ app.js
@@ -0,0 +1,9 @@
+var $toggle = document.querySelector('#nav-toggle');
+var $menu = document.querySelector('#nav-menu');
+
+$toggle.onclick = function() {
+  $toggle.classList.toggle('is-active');
+  $menu.classList.toggle('is-active');
+};
+
+smoothScroll.init();
